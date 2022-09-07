b08f7480b0dd6827e11278dfcaf9e61c5ab0aedf
dev4 <iamdeveloper.me@gmail.com>
2022-09-08 00:34:58 +0530

    views.py

--- are/apps/speakers/views.py
+++ are/apps/speakers/views.py
@@ -12,21 +12,21 @@ class ViewSpeaker(ListView):
 	model = Speaker
 	template_name = "speaker.html"
 
-# class SpeakerCreateView(CreateView):
-# 	model = Speaker
-# 	form_class = SpeakerForm
+class SpeakerCreateView(CreateView):
+	model = Speaker
+	form_class = SpeakerForm
 
-def AddSpeaker(request):
-	if request.method == 'POST' :
-		name = request.POST.get("name")
-		designation = request.POST.get("designation")
-		detail = request.POST.get("detail")
-		profile_image = request.POST.get("profile_image")
-		speaker_register = Speaker(name=name,designation=designation,detail=detail,profile_image=profile_image)
-		speaker_register.save()
+	def AddSpeaker(request):
+		if request.method == 'POST' :
+			name = request.POST.get("name")
+			designation = request.POST.get("designation")
+			detail = request.POST.get("detail")
+			profile_image = request.POST.get("profile_image")
+			speaker_register = Speaker(name=name,designation=designation,detail=detail,profile_image=profile_image)
+			speaker_register.save()
 
 		return redirect("speakers/speaker")
 
-	# def get_success_url(self, **kwargs):
-	# 	return self.object.get_absolute_url()
+	def get_success_url(self, **kwargs):
+		return self.object.get_absolute_url()
 
