f340b38aa77cc0fc5bbd958065fde83750b6899d
dev4 <iamdeveloper.me@gmail.com>
2022-09-09 00:27:02 +0530

    agenda list show

--- are/apps/agenda/urls.py
+++ are/apps/agenda/urls.py
@@ -3,13 +3,14 @@ from . import views
 
 app_name = 'agenda'
 urlpatterns = [
-	path('',views.ConferenceView.as_view(),name='conference_list'),
+	# path('',views.ConferenceView.as_view(),name='conference_list'),
 	path('conferenceadd/',views.ConferenceCreateView.as_view(),name='conference_add'),
-	path('',views.AgendaView.as_view(),name='agenda_list'),
+	path('',views.ConferenceListView.as_view(),name='conference_list'),
+	path('agenda_add/',views.AgendaCreateView.as_view(),name='agenda_add'),
+	path('',views.AgendaListView.as_view(),name='agenda_list'),
 	# path('addagenda/',views.AgendaCreateView.as_view(),name='agenda_add'),
 	# path('conferencedelete/',views.ConferenceDeleteView.as_view(),name='conference_delete'),
 	# path('conferenceupdate/',views.ConferenceUpdateView.as_view(),name='conference_update'),
 	# path('deleteagenda/',views.AgendaDeleteView.as_view(),name='delete_agenda'),
 	# path('updateagenda/',views.AgendaUpdateView.as_view(),name='update_agenda'),
-	path('agenda_add/',views.AddAgenda,name='agenda_add'),
 	]
--- are/apps/agenda/views.py
+++ are/apps/agenda/views.py
@@ -7,57 +7,84 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 # Create your views here.
 
-class ConferenceView(ListView):
-	form_class = AgendaForm
-	template_name = 'agenda.html'
-	# success_url = reverse_lazy('agenda:agenda')
-
 
 class ConferenceCreateView(CreateView):
-	model = Conference
-	form_class = ConferenceForm
-
-	def get_success_url(self, **kwargs):
-		return self.object.get_absolute_url()
+	# import pdb;pdb.set_trace()
+	model = Conference 
+	fields = ['title', 'startdate','enddate']
 
 
+class ConferenceListView(ListView):
+    model = Conference
+    temlate_name = 'agenda.html'
 
-class AgendaView(ListView):
-	model = Agenda
-	template_name = 'agenda.html'
-	# import pdb; pdb.set_trace()
-	# queryset = Agenda.objects.all()
+class AgendaCreateView(CreateView):
+	# import pdb;pdb.set_trace()
+	model = Agenda 
+	fields = ['session', 'starttime','endtime','duration','event']
+
+
+class AgendaListView(ListView):
+    model = Agenda
+    temlate_name = 'agenda.html'
+
+
+
+
+
+# class ConferenceView(ListView):
+# 	form_class = AgendaForm
+# 	template_name = 'agenda.html'
+# 	# success_url = reverse_lazy('agenda:agenda')
+
+
+# class ConferenceCreateView(CreateView):
+# 	model = Conference
+# 	form_class = ConferenceForm
+
+# 	def get_success_url(self, **kwargs):
+# 		return self.object.get_absolute_url()
+
+
+
+
+# class AgendaView(ListView):
+# 	model = Agenda
+# 	template_name = 'agenda.html'
+# 	# import pdb; pdb.set_trace()
+# 	# queryset = Agenda.objects.all()
 	
-	def get_context_data(self, **kwargs):
-		# import pdb; pdb.set_trace()
-		context = super().get_context_data(**kwargs)
-		context['reports_list'] = Agenda.objects.all().order_by('id')
-		return context
-	# def get_queryset(self):
-	# 	import pdb; pdb.set_trace()
-	# 	return Agenda.objects.all().order_by('id')
+# 	def get_context_data(self, **kwargs):
+# 		# import pdb; pdb.set_trace()
+# 		context = super().get_context_data(**kwargs)
+# 		context['reports_list'] = Agenda.objects.all().order_by('id')
+# 		return context
+# 	def get_queryset(self):
+# 		import pdb; pdb.set_trace()
+# 		return Agenda.objects.all().order_by('id')
 
-	# def get_context_data(self, **kwargs):
-	# 	import pdb; pdb.set_trace()
-	# 	context = super().get_context_data(**kwargs)
-	# 	context = Agenda.objects.all().order_by('id')
-	# 	return {'context':context}
+# 	def get_context_data(self, **kwargs):
+# 		import pdb; pdb.set_trace()
+# 		context = super().get_context_data(**kwargs)
+# 		context = Agenda.objects.all().order_by('id')
+# 		return {'context':context}
 
 
-def AddAgenda(request):
-	if request.method == 'POST' :
-		session = request.POST.get("session")
-		starttime = request.POST.get("starttime")
-		endtime = request.POST.get("endtime")
-		duration = request.POST.get("duration")
-		event = request.POST.get("event")
-		agenda_register = Agenda(session=session,starttime=starttime,endtime=endtime,duration=duration,event=event)
-		agenda_register.save()
+# def AddAgenda(request):
+# 	if request.method == 'POST' :
+# 		# import pdb;pdb.set_trace()
+# 		session = request.POST.get("session")
+# 		starttime = request.POST.get("starttime")
+# 		endtime = request.POST.get("endtime")
+# 		duration = request.POST.get("duration")
+# 		event = request.POST.get("event")
+# 		agenda_register = Agenda(session=session,starttime=starttime,endtime=endtime,duration=duration,event=event)
+# 		agenda_register.save()
 
-	return redirect("agenda:agenda_list")
+# 	return redirect("agenda:agenda_list")
 
-	def get_success_url(self, **kwargs):
-		return self.object.get_absolute_url()
+	# def get_success_url(self, **kwargs):
+	# 	return self.object.get_absolute_url()
 
 	
 
