551de3457aa86cd55487c2af32e992fe99552b17
Dev3 <iamdeveloper.me@gmail.com>
2022-09-09 18:03:23 +0530

    edit model

--- are/apps/awards/models.py
+++ are/apps/awards/models.py
@@ -43,6 +43,7 @@ class AwardSubCategory(models.Model):
 	guidelines = RichTextField(blank=True, null=True)
 
 class AmgAward(models.Model):
+	award_type = models.ForeignKey(Award, related_name='award_type', on_delete=models.CASCADE)
 	is_admin = models.BooleanField(default=False)
 	email = models.EmailField(unique=True, blank=True)
 	work_status = models.CharField(max_length=255,choices=WORK_CHOICES,null=True, blank=True)
