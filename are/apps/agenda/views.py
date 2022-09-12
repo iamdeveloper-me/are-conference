d6ffa78d2bd903d0e72613b12df91ae134b0c7b1
Dev2 <iamdeveloper.me@gmail.com>
2022-09-12 13:40:29 +0530

    agenda add on agenda.html done

--- are/apps/agenda/views.py
+++ are/apps/agenda/views.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView,View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.utils.timezone import now
-import datetime
 # Create your views here.
 
 
@@ -23,18 +22,21 @@ class MyFormView(CreateView):
 	form_class = AgendaForm
 	template_name = 'agenda.html'
 
-	def post(self,request,pk,*args,**kwargs):
+
+
+	def agenda_new(request,pk):
+		# conf = get_object_or_404(Conference, pk=pk)
 		obj = Conference.objects.filter(id=kwargs['pk'])[0]
 		# import pdb;pdb.set_trace()        
 		if request.method == 'POST':
-			session = request.POST.get("session")
-			starttime = request.POST.get("starttime")
-			endtime = request.POST.get("endtime")
-			duration = request.POST.get("duration")
-			event = request.POST.get("event")
-			agenda_add=Agenda(session=session,starttime=starttime,endtime=endtime,duration=duration,event=event,obj=obj)						
-			agenda_add.save()
-		return redirect("agenda")
+			form = AgendaForm(request.POST)
+			if form.is_valid():
+				agenda = form.save(commit=False)
+				agenda.save()
+				return redirect('agenda.html',pk=conference.id)
+		else:
+			form = AgendaForm(pk=conference.id)
+		return render(request, 'agenda.html', {'form': form})
 
 	def get_success_url(self, **kwargs):
 		return self.object.get_absolute_url()
@@ -44,15 +46,13 @@ class AgendaView(ListView):
 	# context_object_name = 'name'
 	template_name = 'agenda.html'
 	queryset = Conference.objects.all()
-	
 
 	def get_context_data(self, **kwargs):
 		# import pdb; pdb.set_trace()
 		context = super(AgendaView, self).get_context_data(**kwargs)
 		if len(self.queryset)>0:
-			# now = datetime.datetime.now().date()
-			# conf_list = Conference.objects.filter(date__gt=now()).reverse()[0]
 			context['conf'] = self.queryset[0]
+
 		else:
 			context['conf'] = None
 		context['agenda'] = Agenda.objects.all()
