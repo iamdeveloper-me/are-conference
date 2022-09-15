8a5c3ae975bdc6623716e5317e3fcd0609896381
dev4 <iamdeveloper.me@gmail.com>
2022-09-15 11:58:59 +0530

    time duration complete

--- are/apps/agenda/templatetags/filter.py
+++ are/apps/agenda/templatetags/filter.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime
 register = template.Library()
 
 @register.filter
@@ -10,7 +11,17 @@ def next(some_list, current_index):
 
 @register.filter
 def mytimesince(starttime,endtime):
+    # import pdb; pdb.set_trace()
     end_minutes = endtime.hour*60 + endtime.minute
     start_minutes = starttime.hour*60 + starttime.minute
-    t1 = datetime.strptime(start_time, "%H:%M")
-    return (end_minutes - start_minutes) / 60
+    temp = (end_minutes - start_minutes)/60
+    hours = int(temp)
+    minutes = int((temp-hours)*60)
+    if minutes == 0:
+        time_diff = str(hours) + " hr"
+    elif hours == 0:
+        time_diff = str(minutes) + " min"
+    else:
+        time_diff = str(hours) + " hr " + str(minutes) + " min"
+
+    return time_diff
