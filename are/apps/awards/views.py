e3f520f38cc172e1fc7205a4bf345ba017a824b0
dev <iamdeveloper.me@gmail.com>
2023-07-31 17:34:45 +0530

    update code for issue

--- are/apps/awards/views.py
+++ are/apps/awards/views.py
@@ -250,16 +250,21 @@ class SponsorView(ListView):
 
 	def get_context_data(self,*args, **kwargs):
 		# import pdb; pdb.set_trace()
+		year_list =  SponsorRegister.objects.distinct('year').order_by('year').values("year")
+		sponsor_data = {}
+
+		for ind_year in year_list:
+			sponsor_data[ind_year['year']] = SponsorRegister.objects.filter(year=ind_year['year'])
 		if 'status' in self.request.session and self.request.session['status']=='done':
 			context = {
 				'status':'yes',
-				'data': SponsorRegister.objects.all()
+				'data': sponsor_data
 				}
 			del self.request.session['status']
 		else:
 			context = {
 				'status':'no',
-				'data':SponsorRegister.objects.all()
+				'data':sponsor_data
 				}
 		return context
 
